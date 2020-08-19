bfabfdd7485a6fa35719946ef311572a8b8665a6
Øyvind Wagner <oyvind.wagner@gmail.com>
2020-08-19 08:27:16 +0000

    Corrected spelling error in code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import objectid
+from bson.objectid import ObjectId 
 from os import path
 if path.exists("env.py"):
     import env
