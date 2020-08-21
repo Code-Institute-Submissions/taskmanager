74b91b91c3c13336a687be92303789a6e743b8f7
Øyvind Wagner <oyvind.wagner@gmail.com>
2020-08-21 07:01:14 +0000

    Fixed invalid syntax in app.py

--- app.py
+++ app.py
@@ -68,11 +68,11 @@ def get_categories():
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
-    return render_template('editcategory.html'
+    return render_template('editcategory.html',
     category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
 @app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id)
+def update_category(category_id):
     mongo.db.categories.update(
         {'_id': ObjectId(category_id)},
         {'category_name': request.form.get('category_name')})
